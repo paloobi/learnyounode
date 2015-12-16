e714f15d3667f33fe3bb39e7db33f063de8509da
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2015-12-16 13:10:30 -0800

    remove callback to console.log

--- myFirstAsyncI_O.js
+++ myFirstAsyncI_O.js
@@ -1,16 +1,16 @@
 var fs = require("fs");
 
-function countLines(file, callback) {
+function countLines(file) {
   var lines = "";
   var contents = fs.readFile(file, 'utf-8', function(err, data) {
     if (err) {
       console.log(err);
     } else {
       lines += data;
-      callback(lines.split("\n").length - 1);
+      console.log(lines.split("\n").length - 1);
     }
   }); 
 }
 
 
-countLines(process.argv[2], console.log);
+countLines(process.argv[2]);
